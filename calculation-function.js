201dbf6643140b2292668e7bcab7005a5a49fe09
MJ Bagunas <janevillagracia0@gmail.com>
2023-06-16 20:59:14 +0800

    Manual Testing for Calculation Functions

---
+++ calculation-function.js
@@ -0,0 +1,23 @@
+const num1 = 10;
+const num2 = 5;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+console.log("Addition:", add(num1, num2)); // Expected output: 15
+console.log("Subtraction:", subtract(num1, num2)); // Expected output: 5
+console.log("Multiplication:", multiply(num1, num2)); // Expected output: 50
+console.log("Division:", divide(num1, num2)); // Expected output: 2
